bfd80673a6af8cf51c537a9b861e46c0920ca3e7
Sougata Das <sougatadasabd@gmail.com>
2026-09-17 15:25:40 +0530

    Add root route

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Smart Waste Bin Server is running!');
+});
+
 app.use('/api', sensorRoutes);
 
 const PORT = process.env.PORT || 3001;
